608dd55add3879e5da51f9cf20ac8f2bbfe6692c
Kris Logemann <krisrlogemann@gmail.com>
2019-07-08 15:16:46 -0700

    changed app.js to fix heroku

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+require('dotenv').config();
 
 mongoose.connect('mongodb://test:gungagunga1@ds349247.mlab.com:49247/heroku_dv5d3v7b', { useNewUrlParser: true });
 mongoose.Promise = Promise;
@@ -12,7 +13,7 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    res.status(200).send();
+    res.status(200).send("Gunga");
 });
 
 app.use('/api/users', require('./routes/users'));
